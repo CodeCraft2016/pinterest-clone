dc555c9b4d232e79d1acca17bfe27e3d2a03d064
CodeCraft2016 <136589932+CodeCraft2016@users.noreply.github.com>
2023-08-10 17:21:23 +0200

    add KnpTimeBundle filter & u truncate

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Fidry\PsyshBundle\PsyshBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
 ];
